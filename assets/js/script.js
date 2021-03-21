c4667ffdc5c7ccc89ccd462a264a64f0610cada8
jasonmueller <jmuel9999@gmail.com>
2021-03-21 17:52:29 -0500

    Mostly complete, working out a a bug currently with number of characters created for password.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
  
 // Assignment code here
 
-var randomGen = {
-  lowerCase: getLowerCase,
-  upperCase: getUpperCase,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+// var randomGen = {
+//   lowerCase: getLowerCase,
+//   upperCase: getUpperCase,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol
+// };
 
 function getLowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -24,53 +24,90 @@ function getRandomSymbol() {
   var symbols = "!@#$%^&*(){}[]=<>/,.'"
   return symbols [Math.floor(Math.random() * symbols.length)]
 }
-// console.log(getRandomNumber());
-// console.log(getLowerCase());
-// console.log(getUpperCase());
-// console.log(getRandomSymbol());
 
-  function generatePassword() {
-    //This allows the generator 
-    //var randomChar = "";
-    var numCharacters = window.prompt("How many characters would you like your password to be? Password must be between 8 and 128 characters.");
-    if (numCharacters < 8 || numCharacters > 128) {
-      alert("You must choose between 8 and 128 characters.")
-      return;
-    }  
-    //Does user have lower case characters?
-      var hasLowerCase = confirm("Would you like to include lower case letters? Click 'OK' to confirm."); 
-    //Does user have upper case characters?
-      var hasUpperCase = confirm("Would you like to include upper case letters? Click 'OK' to confirm.");
-    //Does user have random numbers?
-      var hasRandomNumber = confirm("Would you like to include random numbers? Click 'OK' to confirm.");
-    //Does user have random symbols?
-      var hasRandomSymbol = confirm("Would you like to include random symbols? Click 'OK' to confirm.");
-         if (!hasLowerCase && !hasUpperCase && !hasRandomNumber && !hasRandomSymbol) {
+var lowerCase = getLowerCase();
+var upperCase = getUpperCase();
+var randomNumber = getRandomNumber();
+var randomSymbol = getRandomSymbol();
+
+function gatherCharacters() {
+  //;debugger
+var numCharacters = window.prompt("How many characters would you like your password to be? Password must be between 8 and 128 characters.");
+if (numCharacters < 8 || numCharacters > 128) {
+  alert("You must choose between 8 and 128 characters.")
+  return;
+}  
+console.log(numCharacters);
+//Does user have lower case characters?
+  var hasLowerCase = confirm("Would you like to include lower case letters? Click 'OK' to confirm."); 
+//Does user have upper case characters?
+  var hasUpperCase = confirm("Would you like to include upper case letters? Click 'OK' to confirm.");
+//Does user have random numbers?
+  var hasRandomNumber = confirm("Would you like to include random numbers? Click 'OK' to confirm.");
+//Does user have random symbols?
+  var hasRandomSymbol = confirm("Would you like to include random symbols? Click 'OK' to confirm.");
+     if (!hasLowerCase && !hasUpperCase && !hasRandomNumber && !hasRandomSymbol) {
           alert("You must select at least one type of character for your password!");
           return;      
     }
-      //The charStyles object to hold the random character selections
-      var charStyles = {
-        //numCharacters: numCharacters,
-        hasLowerCase: hasLowerCase,
-        hasUpperCase: hasUpperCase,
-        hasRandomNumber: hasRandomNumber,
-        hasRandomSymbol: hasRandomSymbol
-      };
-      return(charStyles);
 
-     /*if(charStyles.hasLowerCase) {
-       var ra
-     } */
+  // console.log(getRandomNumber());
+  // console.log(getLowerCase());
+  // console.log(getUpperCase());
+  // console.log(getRandomSymbol());
 
-    console.log(generatePassword);
-    // var finalPassword = (generatePassword);
-    //var remainingCharacters = charLength - userPassword.length;
-    // generatePassword();
+  //The charStyles object to hold the random character selections
+  var passwordCharStyles = {
+    numCharacters: numCharacters,
+    hasLowerCase: hasLowerCase,
+    hasUpperCase: hasUpperCase,
+    hasRandomNumber: hasRandomNumber,
+    hasRandomSymbol: hasRandomSymbol
+  };
+  return passwordCharStyles;
 }
 
-function randomInfo() {
+function randomInfo(objectArray) {
+  var randomObjectPick = Math.floor(Math.random() * objectArray.length);
+  var randomObjectEl = objectArray[randomObjectPick];
 
+  return randomObjectEl;
+
+}
+
+function generatePassword() {
+  var randomCharStr = "";
+  var charChoices = gatherCharacters();
+  var result = [];
+  var viableChars = [];
+  var actualChars = [];
+
+  //Had to globally declare the variables from up top before the first function
+  if (charChoices.hasLowerCase) {
+    viableChars = viableChars.concat(lowerCase);
+    actualChars.push(randomInfo(lowerCase));
+  }
+  if (charChoices.hasUpperCase) {
+    viableChars = viableChars.concat(upperCase);
+    actualChars.push(randomInfo(upperCase));
+  }
+  if (charChoices.hasRandomNumber) {
+    viableChars = viableChars.concat(randomNumber);
+    actualChars.push(randomInfo(randomNumber));
+  }
+  if (charChoices.hasRandomSymbol) {
+    viableChars = viableChars.concat(randomSymbol);
+    actualChars.push(randomInfo(randomSymbol));
+  }
+  for (var i = 0; i < charChoices.length; i++) {
+    var viableChars = randomInfo(viableChars);
+    result.push(viableChars);
+  }
+  for (var i = 0; i < actualChars.length; i++) {
+    result[i] = actualChars[i];
+  }
+  return result.join(randomCharStr);
+  
 }
 
 // Get references to the #generate element
